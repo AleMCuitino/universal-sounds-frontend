d115d5052f027cd0fafa568c0f0794ff0c381f02
Your Name <iyanperezmarcos@gmail.com>
2023-02-22 15:15:15 +0100

    footer declared

---
+++ src/components/navbar/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <img src="" alt="Facebook logo" />
+                <img src="" alt="Twitter logo" />
+                <img src="" alt="Instagram logo" />
+            </div>
+            
+        </footer>
+    )
+}
+
+export default Navbar
